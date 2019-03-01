6349e3e01c94182829dd548a4d1a25a8b1193264
iq7 <iq_se7en@mail.ru>
2019-03-01 18:16:57 +0300

    Разместил элементы на stiryboard

---
+++ millionerGB/GameViewController.swift
@@ -0,0 +1,54 @@
+//
+//  GameViewController.swift
+//  millionerGB
+//
+//  Created by Андрей Тихонов on 01/03/2019.
+//  Copyright © 2019 Андрей Тихонов. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var QuestionLabel: UILabel!
+    
+    @IBOutlet weak var AnswerAButton: UIButton!
+    @IBOutlet weak var AnswerBButton: UIButton!
+    @IBOutlet weak var AnswerCButton: UIButton!
+    @IBOutlet weak var AnswerDButton: UIButton!
+    
+    @IBOutlet weak var FiftyFiftyButton: UIButton!
+    @IBOutlet weak var CallToFriendButton: UIButton!
+    @IBOutlet weak var HallHelpButton: UIButton!
+    @IBOutlet weak var MarginForErrorButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func AnswerAButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func AnswerBButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func AnswerCButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func AnswerDButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func FiftyFiftyButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func CallToFriendButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func HallHelpButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func MarginForErrorButtonPressed(_ sender: Any) {
+    }
+}
+
