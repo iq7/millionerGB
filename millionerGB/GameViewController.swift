d27c19f595068d62ebe0a436f3a687b0af6b2cad
iq7 <iq_se7en@mail.ru>
2019-03-02 11:45:07 +0300

    Подправил правильные ответы

--- millionerGB/GameViewController.swift
+++ millionerGB/GameViewController.swift
@@ -34,37 +34,37 @@ class GameViewController: UIViewController {
                                   answer: .answerA))
         questions.append(Question(textQuestion: "Что говорит человек, когда замечает нечто необычное?",
                                   arrayAnswers: ["попало в лоб", "залетело в рот", "накапало в уши", "бросилось в глаза"],
-                                  answer: .answerB))
+                                  answer: .answerD))
         questions.append(Question(textQuestion: "Что помогает туристу ориентироваться в незнакомом городе?",
                                   arrayAnswers: ["путепровод", "путеукладчик", "путеводитель", "путеводная звезда"],
                                   answer: .answerC))
         questions.append(Question(textQuestion: "Какой наряд прославил баснописец Крылов?",
                                   arrayAnswers: ["тришкин кафтан", "ивашкин армяк", "прошкин зипун", "машкин сарафан"],
-                                  answer: .answerD))
+                                  answer: .answerA))
         questions.append(Question(textQuestion: "Как звали старшую сестру императора Петра Первого?",
                                   arrayAnswers: ["Вера", "Надежда", "Любовь", "Софья"],
                                   answer: .answerD))
         questions.append(Question(textQuestion: "Что не бывает морским?",
                                   arrayAnswers: ["рельс", "огурец", "гребешок", "узел"],
-                                  answer: .answerD))
+                                  answer: .answerA))
         questions.append(Question(textQuestion: "Кого с большим основанием можно назвать островитянами?",
                                   arrayAnswers: ["алеутов", "эвенков", "чукчей", "якутов"],
-                                  answer: .answerD))
+                                  answer: .answerA))
         questions.append(Question(textQuestion: "В какой стране появилась мандолина?",
                                   arrayAnswers: ["Испания", "Италия", "Венгрия", "Греция"],
-                                  answer: .answerD))
+                                  answer: .answerB))
         questions.append(Question(textQuestion: "Как жители Лондона прозвали небоскреб Мэри-Экс, спроектированный Норманом Фостером?",
                                   arrayAnswers: ["«корнишон»", "«баклажан»", "«кабачок»", "«патиссон»"],
-                                  answer: .answerD))
+                                  answer: .answerA))
         questions.append(Question(textQuestion: "Какой врач первым в истории русской медицины применил гипсовую повязку?",
                                   arrayAnswers: ["Субботин", "Пирогов", "Боткин", "Склифосовский"],
-                                  answer: .answerD))
+                                  answer: .answerB))
         questions.append(Question(textQuestion: "Где в Древней Греции можно было увидеть надпись: «Здесь живут мертвые и говорят немые»?",
                                   arrayAnswers: ["на кладбищах", "в больницах", "в библиотеках", "в тюрьмах"],
-                                  answer: .answerD))
+                                  answer: .answerC))
         questions.append(Question(textQuestion: "Кто был одним из авторов сценария фильма «Музыкальная история» с Сергеем Лемешевым в главной роли?",
                                   arrayAnswers: ["Илья Ильф", "Евгений Петров", "Михаил Зощенко", "Аркадий Аверченко"],
-                                  answer: .answerD))
+                                  answer: .answerB))
         questions.append(Question(textQuestion: "С чем часто охотятся на рыбу протоптера между сезонами дождей?",
                                   arrayAnswers: ["с сетями", "с сачками", "с ружьями", "с лопатами"],
                                   answer: .answerD))
@@ -73,7 +73,7 @@ class GameViewController: UIViewController {
                                   answer: .answerD))
         questions.append(Question(textQuestion: "Что такое лобогрейка?",
                                   arrayAnswers: ["жнейка", "шапка", "болезнь", "печка"],
-                                  answer: .answerD))
+                                  answer: .answerA))
     }
 
     @IBAction func answerAButtonPressed(_ sender: Any) {
