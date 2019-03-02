b2be3c1f7e154714685450c5571ee389d70788cf
iq7 <iq_se7en@mail.ru>
2019-03-02 10:26:24 +0300

    Добавил экран главного меню

---
+++ millionerGB/MainMenuViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainMenuViewController.swift
+//  millionerGB
+//
+//  Created by Андрей Тихонов on 02/03/2019.
+//  Copyright © 2019 Андрей Тихонов. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
