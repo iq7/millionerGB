3cb53fa5879b043ce366d26224811bdf80688a05
iq7 <iq_se7en@mail.ru>
2019-03-07 00:28:01 +0300

    Сделал подсказки

---
+++ millionerGB/Objects/Hints.swift
@@ -0,0 +1,9 @@
+//
+//  Hints.swift
+//  millionerGB
+//
+//  Created by Андрей Тихонов on 06/03/2019.
+//  Copyright © 2019 Андрей Тихонов. All rights reserved.
+//
+
+import Foundation
---
+++ millionerGB/Objects/NumberAnswer.swift
@@ -0,0 +1,9 @@
+//
+//  NumberAnswer.swift
+//  millionerGB
+//
+//  Created by Андрей Тихонов on 06/03/2019.
+//  Copyright © 2019 Андрей Тихонов. All rights reserved.
+//
+
+import Foundation
